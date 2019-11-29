3368916920dcd0b2ff285f8c6abffd62000f6c0a
AkashM398 <akashm1999@gmail.com>
2019-11-29 09:26:40 +0530

    added files

---
+++ hackerrank/Solved/Initialisation.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int i,j,n;
+    n=10;
+    int arr[n];
+    
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2!=0)
+        {
+            printf("%d ",arr[i]);
+        }
+    }
+    return 0;
+}
