f2ca8dc7a74bd7bf7af1cc5f00075497fe9a32fe
AkashM398 <akashm1999@gmail.com>
2019-11-29 09:35:38 +0530

    added Questions

--- hackerrank/Solved/Array-1.c
+++ hackerrank/Solved/Array-1.c
@@ -1,3 +1,36 @@
+/*
+Question:
+
+An array is one of the most useful data structures provided by C. It is a collection of data that holds a fixed number of values of same type.
+
+Declaration :
+
+<data_type> <array_name>[size].
+For example :
+
+int arr[10] 
+will create an array of name arr that holds 10 integers
+
+char chars[25]
+will create an array of name chars that holds 25 characters
+
+The data in an array is 0-indexed, i.e. the first element is arr[0], second element is arr[1] and so on.
+
+You can access the ith element by using arr[i - 1], hence the last index is N - 1 where N is size.
+
+For this task, complete the code to input an array of 5 numbers and print it.
+
+Sample Input 0
+
+5 6 1 2 7
+
+Sample Output 0
+
+5 6 1 2 7
+*/
+
+//Solution
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
--- hackerrank/Solved/Using Variable as an Array Size 1.c
+++ hackerrank/Solved/Using Variable as an Array Size 1.c
@@ -1,3 +1,28 @@
+/*
+Question: 
+
+So far we used a constant to declare the size of the array : arr[10] or arr[5].
+
+We can also use a variable instead : arr[N], or arr[size] etc.
+
+We just need to make sure our variable holds an appropriate value before we it to declare the array.
+
+Write a program to input a number N, the size of the array.
+
+And then to input N integers, the elements of the array and print them in reverse order.
+
+Sample Input 0
+
+6
+1 2 3 4 5 6
+
+Sample Output 0
+
+6 5 4 3 2 1
+*/
+
+//Solution
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
